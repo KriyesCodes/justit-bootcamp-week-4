fa575d74ab21abebe73d6cb524109575d0f2a8c1
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2023-10-09 10:22:27 +0100

    Add examples of data types in js

---
+++ Day 1/example/script.js
@@ -0,0 +1,29 @@
+// Properties - Data or information about the object
+
+// Methods - Things the object can do
+
+console.log("Hello World");
+
+// Data Types
+
+// String - Represents characters enclosed in quote marks
+console.log("I am a string!");
+
+// Numbers
+console.log(100);
+
+// Boolean - True or False
+console.log(true);
+console.log(false);
+
+// REMEMBER numbers inside quote marks are strings
+console.log("1000");
+
+// Null - An abscence of any value
+console.log(null);
+
+// Undefined - Data without a defined type
+console.log(undefined);
+
+// NaN - Not a number
+console.log(NaN);

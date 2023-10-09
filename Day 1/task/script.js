27d6a908de0c7d84e43964b1908d7ddf0571f3d8
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2023-10-09 11:42:18 +0100

    Add boilerplate for task

---
+++ Day 1/task/script.js
@@ -0,0 +1,7 @@
+/*
+Tasks:
+
+1: Create a variable that stores your name and log a string to the console that personally greets you using template literals. 
+2: Create variables storing your name, your favourite colour and your favourite film or tv series. Using template literals I want you to log a sentence including these values to the console.
+3: Create variables storing what you ate for breakfast, lunch and dinner yesterday and log them to the console. Without redeclaring new variables I would like you to assign a new value of what you ate today to the variables, and again log them to the console.
+*/

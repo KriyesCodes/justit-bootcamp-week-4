a9dabb2f3182b22583fe00085d2429cf9d24cb56
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2023-10-10 11:42:46 +0100

    Complete day 2 exercises

--- Day 2/exercises/script.js
+++ Day 2/exercises/script.js
@@ -8,3 +8,65 @@ Task:
 
 4: Create a variable that stores a number. If the number is divisible by 3, log "fizz" to the console. If the number is divisible by 5 log "buzz" to the console. If the number is divisible by both 3 and 5, log "fizz buzz" to the console. Otherwise log the number to the console.
 */
+
+// task 1
+console.log(`Task 1:
+-----`);
+
+let age = 19;
+console.log(`Input: ${age}`);
+
+if (age >= 18) {
+  console.log("No problem, what would you like to drink?");
+}
+else {
+  console.log("Sorry, I can't serve you");
+}
+
+// task 2
+console.log(`Task 2:
+-----`);
+
+let password = "NB}IOBbObi8";
+console.log(`Input: ${password}`);
+
+if (password.length >= 8) {
+  console.log(password);
+}
+else {
+  console.log("Password is too short");
+}
+
+// task 3
+console.log(`Task 3:
+-----`);
+
+let num = 9;
+console.log(`Input: ${num}`);
+
+if (num % 3 === 0 || num % 5 === 0) {
+  console.log("This number is divisible by 3 or 5");
+}
+else {
+  console.log("This number is neither divisible by 3 nor 5");
+}
+
+// task 4
+console.log(`Task 4:
+-----`);
+
+num = 15;
+console.log(`Input: ${num}`);
+
+if (num % 3 === 0 && num % 5 === 0) {
+  console.log("fizz buzz");
+}
+else if (num % 3 === 0) {
+  console.log("fizz");
+}
+else if (num % 5 === 0) {
+  console.log("buzz");
+}
+else {
+  console.log(num);
+};

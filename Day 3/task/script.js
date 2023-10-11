9cbb829f332fa3b4b2a7f05178a911531e2bd948
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2023-10-11 11:43:14 +0100

    Add task boilerplate

---
+++ Day 3/task/script.js
@@ -0,0 +1,11 @@
+/*
+Tasks: 
+
+1: Create an array of your favourite Films / TV shows, up to 5 items. Use an array method to add 2 more items to your array. Use a loop to cycle through the array and log each item to the console.
+
+2: Write a loop to generate 10 random numbers between 1-100 and log them to the console.
+
+3: Write a loop that counts backwards from 20-0, logging each number to the console.
+
+4: Generate 5 random numbers between 1-50. For each number generated, check if the number is divisible by 5 or not. Log whether it is divisible or not to the console.
+*/

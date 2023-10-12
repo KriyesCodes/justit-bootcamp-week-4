97e6b4110a9795eee263b9db469aca4b82f365cf
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2023-10-12 11:47:08 +0100

    Complete day 4 tasks

--- Day 4/task/script.js
+++ Day 4/task/script.js
@@ -9,3 +9,80 @@ Tasks:
 
 4: Research arrow functions to familiarise yourself with the differences between them and function declarations. I would then like you to write some example arrow functions and convert a function from a declaration to an arrow function. 
 */
+
+// Task 1
+console.log("Task 1 \n -----");
+
+function greet(name) {
+  console.log(`Hello there ${name}!`);
+}
+
+greet("kriyes");
+
+// Task 2
+console.log("Task 2 \n -----");
+
+function oddOrEven(num) {
+  if (num % 2 === 0) {
+    console.log(`${num} is even`);
+  }
+  else {
+    console.log(`${num} is odd`);
+  }
+}
+
+oddOrEven(96);
+oddOrEven(7);
+
+// Task 3
+console.log("Task 3 \n -----");
+
+let availableFunds = 500;
+
+function withdrawMoney(pin, requestAmount) {
+  const correctPin = 2093;
+
+  console.log(`Initiating withdrawal process with pin ${pin} and request amount ${requestAmount}:`);
+
+  if (pin === correctPin) {
+    console.log("Pin is correct. Processing...\n");
+
+    if (availableFunds === 0 || requestAmount > availableFunds) {
+      console.log(`Withdrawal Denied. Not enough available funds. Funds available: £${availableFunds}`);
+    }
+    else if ((Math.round(requestAmount * 100) / 100) <= 0) {
+      console.log("Withdrawl Denied. Cannot withdraw a negative, zero, or amount less than 1p.");
+    }
+    else {
+      availableFunds = Math.round(availableFunds * 100 - requestAmount * 100) / 100;
+      console.log(`Withdrawal Successful. £${requestAmount} has been withdrawn. Funds remaining: £${availableFunds}`);
+    }
+  }
+  else {
+    console.log("Access Denied. Pin is incorrect")
+  }
+}
+
+withdrawMoney(1111, 20);
+withdrawMoney(2093, 700);
+withdrawMoney(2093, -200);
+withdrawMoney(2093, 0);
+withdrawMoney(2093, 0.004);
+withdrawMoney(2093, 200);
+withdrawMoney(2093, 96.05);
+
+
+// Task 4
+console.log("Task 4 \n -----");
+
+const greetArrow = (name) => {
+  console.log(`Hello there ${name}!`);
+}
+
+greetArrow("kriyes");
+
+const oddOrEvenImplicitReturn = (num) => console.log((num % 2) ? `${num} is odd` : `${num} is even`);
+
+oddOrEvenImplicitReturn(5);
+oddOrEvenImplicitReturn(8);
+

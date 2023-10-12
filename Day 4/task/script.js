0c497a65485a8c478728db6d80754de377912954
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2023-10-12 11:06:56 +0100

    Add task boilerplate

---
+++ Day 4/task/script.js
@@ -0,0 +1,11 @@
+/*
+Tasks:
+
+1: Write a function that takes in your name as an argument and logs a personalised greeting to you in the console.
+
+2: Write a function that takes in a number as an argument and check whether it is odd or even and logs the result to the console.
+
+3. Write a function that replicates the functionality of Cash Machine / ATM. The ATM should have parameters of pin number and withdrawal amount that are compared to variables of account balance and stored pin number. If the pin passed as an argument is correct and the withdrawal amount is less than the account balance, approve the transaction. If the pin is wrong or the withdrawal amount is greater than the account balance, deny the transaction.
+
+4: Research arrow functions to familiarise yourself with the differences between them and function declarations. I would then like you to write some example arrow functions and convert a function from a declaration to an arrow function. 
+*/

fce3b8040fea5b7f864179028ffb35b7ab56b34d
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2023-10-13 10:52:54 +0100

    Add day 5 task boilerplate

---
+++ Day 5/task/script.js
@@ -0,0 +1,9 @@
+/*
+Tasks
+
+1: Create an Object named "person" including key:value pairs of "name", "age" and "city".
+
+2: Add an array of your favourite films to the person object using dot notation.
+
+3: Create an object named "pet" key value pairs of "animal" and "name". Add methods to the object of "eat" and "drink" that return a message about the pet eating or drinking.
+*/
